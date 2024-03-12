a3e799719f064e81af54d81a5737aca261d163e6
Redaction-F <koebijakobiteorhang@gmail.com>
2024-03-12 22:02:43 +0900

    ver0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,39 @@
-mod sample_1;
-mod sample_2;
+pub struct Struct1 {
+    field_1: u32
+}
 
-pub use sample_1::Struct2;
-pub use sample_2::{Struct3, Struct4};
+impl Struct1 {
+    pub fn new() -> Struct1 {
+        Struct1 { 
+            field_1: 0 
+        }
+    }
+
+    pub fn get_field_1(&self) -> u32 {
+        self.field_1
+    }
+
+    pub fn set_field_1(&mut self, sf1_u32: u32) {
+        self.field_1 = sf1_u32
+    }
+}
+
+pub struct Struct2<'a> {
+    field_1: &'a u32
+}
+
+impl<'a> Struct2<'a> {
+    pub fn new<'b>() -> Struct2<'b> {
+        Struct2 { 
+            field_1: &0
+        }
+    }
+
+    pub fn get_field_1(&self) -> &u32 {
+        self.field_1
+    }
+
+    pub fn set_field_1(&'a mut self, sf1_str: &'a u32) {
+        self.field_1 = sf1_str;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 extern crate sample_for_book;
-use sample_for_book::{Struct2, Struct3, Struct4};
+#[allow(unused_imports)]
+use sample_for_book::{Struct1, Struct2};
 
 fn main() {
-    // let mut struct_3_1: Struct3 = Struct3::new();
-    // struct_3_1.get_field_1_mut().set_field_1(5);
-    // let u32_1: u32 = struct_3_1.get_field_1().get_field_1();
-    // println!("{}", u32_1);
-    let mut struct_4_1: Struct4 = Struct4::new("Sample");
-    {
-        let struct_2_1: &mut Struct2<'_> = struct_4_1.get_field_1_mut();
-        struct_2_1.set_field_1("Change");
-    }
-    // struct_4_1.get_field_1_mut().set_field_1("Change");
-    // let u32_2: &str = struct_4_1.get_field_1().get_field_1();
-    // println!("{}", u32_2);
+    let mut struct_1_1: Struct1 = Struct1::new();
+    struct_1_1.set_field_1(5);
+    println!("{}", struct_1_1.get_field_1());
+    let mut struct_1_1: Struct2 = Struct2::new();
+    struct_1_1.set_field_1(&5);
+    // エエラー発生
+    // println!("{}", struct_1_1.get_field_1());
 }
